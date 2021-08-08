a5b01b7873d23d4442030fd7da796a01b79843cd
tati oliveira <tat1roliv@gmail.com>
2021-08-08 20:40:35 -0300

    tooltip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 
 /*tooltip*/
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+});
